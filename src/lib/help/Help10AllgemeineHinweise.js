14fc75166fc0fcc202b26f1d27e81cdbfc8c3380
hejpecher Janssen <hendrik.janssen@pecher.de>
2024-02-20 07:27:54 +0000

    Allgemeine Hinweise

--- src/lib/help/Help10AllgemeineHinweise.js
+++ src/lib/help/Help10AllgemeineHinweise.js
@@ -23,12 +23,12 @@ const Component = () => {
             align="right"
           />
           <p>
-            Die Starkregengefahrenkarte im AIS Starkregenvorsorge stellt in zwei
+            Die Starkregengefahrenkarte im AIS Starkregenvorsorge Korschenbroich stellt in zwei
             umschaltbaren Kartenansichten maximale Wassertiefen bzw. maximale
             Fließgeschwindigkeiten im gesamten Gültigkeitsgebiet dar, die im
-            Verlauf von drei simulierten Starkregenszenarien berechnet wurden.
+            Verlauf von zwei simulierten Starkregenszenarien berechnet wurden.
             Bei den Ergebnissen handelt es sich um eine modelltechnische
-            Untersuchung des Gültigkeitsgebiets mit dem Ziel die Realität
+            Untersuchung des Stadtgebiets mit dem Ziel die Realität
             möglichst gut abzubilden. Bei der Betrachtung und Interpretation der
             Ergebnisse sind die Modellgrundlagen und -annahmen zu
             berücksichtigen. Hinweise zur Aussagekraft der Ergebnisse werden
@@ -38,7 +38,7 @@ const Component = () => {
           <p>
             Das Ergebnis der Simulation ist von der Dauer und Intensität des
             Regens abhängig, der für die Simulation angenommen wird. Wir bieten
-            Ihnen hierzu drei unterschiedliche{" "}
+            Ihnen hierzu zwei unterschiedliche{" "}
             <a
               className="renderAsLink"
               onClick={() => setAppMenuActiveMenuSection("szenarien")}
@@ -46,26 +46,24 @@ const Component = () => {
               simulierte Szenarien
             </a>{" "}
             an, darunter ein außergewöhnlicher Starkregen (SRI 7) und ein
-            extremer Starkregen (SRI 9 bzw. 10) als "Modellregen" sowie die
-            Abbildung der Niederschlagsbelastung des Ereignisses vom 14.07 und
-            15.07.2021, basierend auf Radardaten.{" "}
+            extremer Starkregen (SRI 11) als "Modellregen".{" "}
           </p>
 
           <p>
             Der Farbverlauf für die <strong>Wassertiefen</strong> nutzt die
-            Eckwerte 20 cm (blau), 40 cm (gelb), 75 cm (orange) und {">"} 100 cm
+            Eckwerte 10 cm (blau), 30 cm (gelb), 50 cm (orange) und 100 cm
             (rot). Wassertiefen unter 5 cm werden nicht mehr farbig ausgeprägt
             (transparente Darstellung). Zur Visualisierung der{" "}
             <strong>Fließgeschwindigkeiten</strong>, angegeben in Meter pro
-            Sekunde (m/s), werden die Eckwerte 0,5 m/s (gelb), 2,0 m/s (orange),
-            4,0 m/s (hellrot) und 6,0 m/s (dunkelrot) verwendet. Der untere
+            Sekunde (m/s), werden die Eckwerte 0,3 m/s (gelb), 0,5 m/s (orange),
+            1,3 m/s (hellrot) und 3,0 m/s (dunkelrot) verwendet. Der untere
             Grenzwert für die farbige Anzeige einer Fließgeschwindigkeit liegt
             bei 0,2 m/s.
           </p>
 
           <p>
-            Die Simulationsberechnungen wurden im Auftrag durch das
-            Ingenieurbüro Dr. Pecher AG (Erkrath/Gelsenkirchen) durchgeführt.
+            Die Simulationsberechnungen wurden durch das
+            Ingenieurbüro Dr. Pecher AG (Erkrath/Gelsenkirchen) im Auftrag der Stadt Korschenbroich durchgeführt.
           </p>
 
           <p>
@@ -92,7 +90,7 @@ const Component = () => {
             können in Teilgebieten die Wassertiefen auf dem Gelände höher oder
             geringer ausfallen. Um diese Variationen zu verstehen und besser
             einschätzen zu können, sind die unterschiedlichen Szenarien
-            dargestellt.
+            dargestellt. Weitere Informationen sind in dem Reiter "Aussagekraft der Simulationen" zu finden.
           </p>
         </div>
       }
