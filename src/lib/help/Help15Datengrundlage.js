e01aaf5efabef7e0e45b1484e5cc34b39ab2dbdd
hejpecher Janssen <hendrik.janssen@pecher.de>
2024-02-20 08:14:49 +0000

    Datengrundlage

--- src/lib/help/Help15Datengrundlage.js
+++ src/lib/help/Help15Datengrundlage.js
@@ -30,29 +30,26 @@ const Component = () => {
 
           <p>
             Die Simulationen wurden mit einem Oberflächenabflussmodell für das
-            hydrologische Einzugsgebiet mit einer Auflösung von 1x1 Meter
+            hydrologische Einzugsgebiet der Stadt Korschenbroich mit einer Auflösung von 1x1 Meter
             aufgebaut. Wesentliche Modellgrundlage ist das Digitale
             Geländemodell (DGM1). Als Grundlage hierfür dienen flächenhafte
             Höhenmessungen, die das Land NRW turnusmäßig mit einem Laserscanner
             aus einem Flugzeug heraus durchführt (Aufnahme durch Laseraltimetrie
-            im Jahr 2021 und 2022). Für die Simulation wurde das DGM1 um die
-            Gebäude aus dem Liegenschaftskataster (10/2022) ergänzt. Außerhalb
-            des Gültigkeitsgebiets wurden Daten des Bundesamts für Kartographie
-            und Geodäsie verwendet (Bundesamt für Kartographie und Geodäsie, ©
-            GeoBasis-DE / BKG (2023)).
+            im Jahr 2022). Für die Simulation wurde das DGM1 um die
+            Gebäude aus dem Liegenschaftskataster (2023) ergänzt.
           </p>
 
           <p>
             Das Oberflächenmodell wurde anschließend um wichtige verrohrte
-            Gewässerabschnitte sowie Geländedurchlässe ergänzt, um eine
-            hydrologisch korrekte Abflussberechnung zu gewährleisten. In diesem
+            Gewässerabschnitte sowie Geländedurchlässe ergänzt, um eine hydrologisch 
+            korrekte Abbildung von potenziellen Fließwegen zu gewährleisten. In diesem
             Rahmen wurden auch weitere Fließhindernisse wie z. B. Mauern ergänzt
             und maßgebliche siedlungswasserwirtschaftliche Bauwerke (Hochwasser-
             und Regenrückhaltebecken) mit ihrer Wirkung im Oberflächenmodell
             abgebildet. Zusätzlich wurden die Gebäude auf Aktualität geprüft und
             abgerissene oder geplante Gebäude gegebenenfalls angepasst. Sehr
             neue Gebäude, die nach dem Modellaufbau fertiggestellt wurden (z. B.
-            Neubaugebiete) sind daher noch nicht im Datenbestand erfasst. Hier
+            Neubaugebiete) sind daher noch nicht im Datenbestand erfasst.  Hier
             lassen sich aus dem angrenzenden Gelände dennoch wichtige Hinweise
             zur möglichen Überflutung ableiten (s. auch Schaltfläche: Fehler im
             Geländemodell melden).
@@ -60,10 +57,8 @@ const Component = () => {
 
           <p>
             Grundlage für die Modellanpassungen waren die kommunal verfügbaren
-            Datensätze, Ortsbegehungen im Kreisgebiet und eine Prüfung durch
-            kommunale Vertreter:innen. Auf Grundlage von Testrechnungen wurde
-            das Modell anschließend gemeinsam mit Vertreter:innen aus den
-            Kommunen geprüft und angepasst.
+            Datensätze, Ortsbegehungen im Stadtgebiet und eine Prüfung durch
+            Mitarbeiterinnen und Mitarbeiter des Städtischen Entworgungsbetriebs Korschenbroich (SEK).
           </p>
 
           <p>Welche Daten wurden berücksichtigt?</p>
@@ -75,10 +70,14 @@ const Component = () => {
               ELWAS-Daten (Datenlizenz Deutschland Zero
               (https://www.govdata.de/dl-de/zero-2-0))
             </li>
-
-            <li>...</li>
+            <li>
+            Stadt Korschenbroich/SEK: Dokumentation Ereignis 2020, ALKIS-Daten, B-Pläne, Vermessungsdaten von Gewässern, Informationen aus dem Generalentwässerungsplan (GEP) zu Überstauschächten, darüber hinaus erfolgte eine weitergehende Datenerfassung und Modellprüfung in einer Online-Karte.
+            </li>
           </ul>
 
+          <p>Flüsse wie die Niers weisen eine relativ hohe Abflussleistung auf und reagieren eher auf langanhaltende und großräumige Niederschläge mit Hochwasser. 
+            Überschwemmungen durch die Niers werden in den Hochwassergefahrenkarten abgebildet. Im AIS dargestellte kurze und lokale Starkregenereignisse (Dauer 60 min) 
+            wirken sich hingegen stärker auf kleinere Gewässer aus und führen in urbanen Räumen zu Überflutungen.</p>
           <p>
             Zur Betrachtung der Ergebnisse stehen drei verschiedene
             Hintergrundkarten zur Verfügung, die auf den folgenden
@@ -107,21 +106,50 @@ const Component = () => {
               (OpenStreetMap contributors).
             </li>
             <li>
-              <strong>Luftbildkarte</strong>: (1) Kartendienst (WMS) des RVR.
+              <strong>Luftbildkarte</strong>: (1) Kartendienst (WMS) des Landes NRW.
               Datengrundlage:{" "}
               <strong>Digitale Orthophotos (DOP) des Landes NRW</strong>{" "}
               WMS-Dienst für farbige, digitale, georeferenzierte, lagegenaue,
               entzerrte Luftbilder des Landes NRW. (
               <a
                 target="_legal"
-                href="https://www.bezreg-koeln.nrw.de/brk_internet/geobasis/luftbildinformationen/aktuell/digitale_orthophotos/index.html"
+                href="https://www.bezreg-koeln.nrw.de/geobasis-nrw/produkte-und-dienste/luftbild-und-satellitenbildinformationen/aktuelle-luftbild-und-0"
               >
                 weiter Informationen
               </a>
-              ). (2) Kartendienste (WMS) des Regionalverbandes Ruhr (RVR).
-              Datengrundlagen: <strong>Stadtkarte 2.0</strong> und{" "}
-              <strong>Kartenschrift aus der Stadtkarte 2.0</strong>. (Details s.
-              Hintergrundkarte Stadtplan).
+              ). (2) Kartendienste (WMS) des Regionalverbandes Ruhr (RVR). 
+              Datengrundlagen: <strong>Stadtkarte 2.0</strong> Wöchentlich in einem automatischen Prozess 
+              aktualisierte Zusammenführung des Straßennetzes der OpenStreetMap mit 
+              Amtlichen Geobasisdaten des Landes NRW aus den Fachverfahren ALKIS 
+              (Gebäude, Flächennutzungen) und ATKIS (Gewässer). © RVR und 
+              Kooperationspartner 
+              (
+              <a
+                target="_legal"
+                href="https://www.govdata.de/dl-de/by-2-0"
+              >
+                Datenlizenz Deutschland - Namensnennung - Version 2.0
+              </a>
+              ). 
+              Lizenzen der Ausgangsprodukte: 
+              (
+              <a
+                target="_legal"
+                href="https://www.govdata.de/dl-de/zero-2-0"
+              >
+                Datenlizenz Deutschland - Zero - Version 2.0
+              </a>
+              ) 
+              (Amtliche Geobasisdaten) und ODbL 
+              (
+              <a
+                target="_legal"
+                href="https://opendatacommons.org/licenses/odbl/1-0/"
+              >
+                OpenStreetMap contributors
+              </a>
+              )
+              .
             </li>
             <li>
               <strong>DTK (bunt)</strong>: DTK Sammeldienst des Landes NRW.
@@ -138,13 +166,16 @@ const Component = () => {
               Nutzungsbedingungen: siehe{" "}
               <a
                 target="_legal"
-                href="http://www.bezreg-koeln.nrw.de/brk_internet/geobasis/lizenzbedingungen_geobasis_nrw.pdf"
+                href="https://www.bezreg-koeln.nrw.de/system/files/media/document/file/lizenzbedingungen_geobasis_nrw.pdf"
               >
                 Nutzungsbedingungen Geobasis NRW
               </a>
               , Für die DTK 250, DTK 500 gelten die Nutzungsbedingungen des BKG:
               ©{" "}
-              <a target="_legal" href="www.govdata.de/dl-de/by-2-0">
+              <a 
+              target="_legal" 
+              href="https://www.govdata.de/dl-de/by-2-0"
+              >
                 GeoBasis-DE / BKG(2020) dl-de/by-2-0
               </a>
             </li>
